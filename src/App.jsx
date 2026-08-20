fea478d7527dad705b2fa659b536b975e2f40949
pattaraponsaksee <pattaraponsaksee@gmail.com>
2026-08-20 15:23:23 +0700

    feat: lift state up to connect CategorySelector and Randomizer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Randomizer from './components/Randomizer'
 import CategorySelector from "./components/CategorySelector";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Food");
+
   return (
     <div className='flex flex-col items-center p-8 bg-white w-full min-h-screen gap-8'>
-      <CategorySelector />
-      <Randomizer />
+      <CategorySelector 
+        selectedCategory={selectedCategory} 
+        onSelectCategory={setSelectedCategory} 
+      />
+      <Randomizer category={selectedCategory} />
     </div>
   )
 }
