58c05c45826d7d441f4fb8ec06153dd8cb2d1699
SupakitPor <prongchais.gmail.com>
2026-08-20 17:04:39 +0700

    update data

--- src/Data/mockdataExercises.js
+++ src/Data/mockdataExercises.js
@@ -2,31 +2,31 @@ export const Exercises = [
 {
   id: "1",
   name: "เดิน / Walking",
-  description: "Calories Burn: 250 kcal/hour",
+  description: "🔥 Calories Burned: 250 kcal/hour",
   image: "🚶‍♀️",
 },
 {
   id: "2",
   name: "วิ่ง / Running",
-  description: "Calories Burn: 560 kcal/hour",
+  description: "🔥 Calories Burned: 560 kcal/hour",
   image: "🏃‍♂️",
 },
 {
   id: "3",
   name: "ขี่จักรยาน / Cycling",
-  description: "Calories Burn: 450 kcal/hour",
+  description: "🔥 Calories Burned: 450 kcal/hour",
   image: "🚴‍♀️",
 },
 {
   id: "4",
   name: "ว่ายน้ำ / Swimming",
-  description: "Calories Burn: 500 kcal/hour",
+  description: "🔥 Calories Burned: 500 kcal/hour",
   image: "🏊‍♀️",
 },
 {
   id: "5",
   name: "ต่อยมวย / Boxing",
-  description: "Calories Burn: 600 kcal/hour",
+  description: "🔥 Calories Burned: 600 kcal/hour",
   image: "🥊",
 },
 ];
