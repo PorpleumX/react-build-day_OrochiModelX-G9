daeea87cbf3218a3481cbb3ced3f0a9ee9a5f31c
saokeems <saokeems@gmail.com>
2026-08-20 11:55:31 +0700

    feat: added CategorySelector with fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import CategorySelector from "./components/CategorySelector";
 
 const App = () => {
-  return (
-    <div className='flex bg-amber-400 w-full min-h-screen'>App</div>
-  )
-}
+  const [selectedCategory, setSelectedCategory] = useState("Food");
 
-export default App
+  const categories = ["Food", "Exercise"];
+
+  const cardsData = [
+    {
+      id: 1,
+      title: "ก๋วยเตี๋ยวเรือ",
+      category: "Food",
+      desc: "แคลอรี่ 350 kcal",
+    },
+    { id: 2, title: "ข้าวมันไก่", category: "Food", desc: "แคลอรี่ 590 kcal" },
+    {
+      id: 3,
+      title: "วิ่งสวนสาธารณะ",
+      category: "Exercise",
+      desc: "เบิร์น 300 kcal",
+    },
+    {
+      id: 4,
+      title: "เวทเทรนนิ่ง",
+      category: "Exercise",
+      desc: "เบิร์น 200 kcal",
+    },
+  ];
+
+  const handleCategory = (cate) => {
+    setSelectedCategory(cate);
+    console.log(cate);
+  };
+
+  const filteredCards = cardsData.filter(
+    (item) => item.category === selectedCategory,
+  );
+
+  return (
+    <div className="flex flex-col bg-slate-200 w-full min-h-screen justify-center items-center">
+      <h1>Date: ../../..</h1>
+      <CategorySelector
+        selectedCategory={selectedCategory}
+        categories={categories}
+        handleCategory={handleCategory}
+        filteredCards={filteredCards}
+      />
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/CategorySelector.jsx
@@ -0,0 +1,34 @@
+const CategorySelector = ({ selectedCategory, categories, handleCategory,filteredCards }) => {
+  return (
+    <>
+      <div className="flex gap-2">
+        {categories.map((item) => (
+          <div key={item} className="mx-2 my-4">
+            <button
+              onClick={() => handleCategory(item)}
+              className={`px-4 py-2 rounded transition-colors ${
+                selectedCategory === item
+                  ? "bg-amber-400 text-white font-bold" // Style ตอนถูกเลือก
+                  : "bg-white text-gray-700 hover:bg-amber-100" // Style ปกติ
+              }`}
+            >
+              {item}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-4 w-full max-w-md">
+        {filteredCards.map((card) => (
+          <div key={card.id} className="bg-white p-4 rounded border">
+            <h2 className="text-lg font-bold text-gray-800">{card.title}</h2>
+            <p className="text-gray-600 text-sm mt-1">{card.desc}</p>
+            <span className="inline-block mt-3 px-2 py-1 bg-amber-100 text-amber-800 text-xs rounded">
+              {card.category}
+            </span>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+export default CategorySelector;
