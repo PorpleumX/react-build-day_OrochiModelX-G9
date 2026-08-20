8c4fdb2d36b2185b86eea4370ccf2bf6e8ca3cb0
SupakitPor <prongchais.gmail.com>
2026-08-20 11:59:27 +0700

    create file FoodRender and TextRender

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import FoodRender from "./components/FoodRender";
+import TextRender from "./components/TextRender";
 
 const App = () => {
-  return (
-    <div className='flex bg-amber-400 w-full min-h-screen'>App</div>
-  )
-}
+    return (
+        <div className="flex flex-col min-w-screen min-h-screen">
+          <nav className="bg-slate-600 text-center h-100">
+            <h2>Nav</h2>
+          </nav>
+          <TextRender />
+        </div>
+    );
+};
 
-export default App
+export default App;
---
+++ src/components/FoodRender.jsx
@@ -0,0 +1,8 @@
+export default function FoodRender() {
+    return (
+        <div className="flex-1 flex flex-col items-center justify-center bg-slate-900 gap-10">
+            <h1>foodList</h1>
+            <button className="bg-amber-800 text-white px-5 py-2 rounded-xl">Button</button>
+        </div>
+    );
+}
